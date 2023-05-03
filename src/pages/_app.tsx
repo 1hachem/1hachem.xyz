1aa3781d52a05bc960baff0e43d5e4309ebbf888
hachem betrouni <hb@big-mama.io>
2023-05-04 00:02:45 +0100

    remove blobs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import Blob from "@/components/Blob";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
 
@@ -9,9 +8,6 @@ import "@/styles/globals.css";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
-      <Blob className="top-10 left-16 bg-purple-300 dark:bg-blue-400" />
-      <Blob className="top-10 right-16 bg-yellow-300 dark:bg-green-400" />
-      <Blob className="bottom-10 left-16 bg-green-300 dark:bg-purple-400" />
       <div className="flex flex-col min-h-screen">
         <Navbar />
         <div className="flex-1">
