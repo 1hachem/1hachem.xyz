fef687440efd1e4a222c405d424dce392a899235
hachem betrouni <hb@big-mama.io>
2023-05-03 22:54:42 +0100

    use framer motion for blob animation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,8 +10,8 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
       <Blob className="top-10 left-16 bg-purple-300 dark:bg-blue-400" />
-      <Blob className="top-10 right-16 bg-yellow-300 dark:bg-green-400 animation-delay-2000" />
-      <Blob className="bottom-10 left-16 bg-green-300 dark:bg-purple-400 animation-delay-4000" />
+      <Blob className="top-10 right-16 bg-yellow-300 dark:bg-green-400" />
+      <Blob className="bottom-10 left-16 bg-green-300 dark:bg-purple-400" />
       <div className="flex flex-col min-h-screen">
         <Navbar />
         <div className="flex-1">
