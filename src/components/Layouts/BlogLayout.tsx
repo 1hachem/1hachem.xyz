e989be3607edde0da4ca5f5b5fd0e048b1dc6b9a
hachem betrouni <hb@big-mama.io>
2023-05-04 00:02:37 +0100

    update blog

--- src/components/Layouts/BlogLayout.tsx
+++ src/components/Layouts/BlogLayout.tsx
@@ -22,11 +22,12 @@ const Layout = ({ children, meta }: LayoutProps) => {
         <title>{meta.title}</title>
         <meta name="description" content={meta.title} />
       </Head>
-      <div className="w-[90%] mx-auto bg-zinc-50 bg-opacity-40 dark:bg-opacity-20 bg-blur-3xl rounded-xl">
-        <h1 className="text-5xl">{meta.title}</h1>
+      <div className="w-[90%] mx-auto p-2">
+        <h1 className="text-5xl py-4">{meta.title}</h1>
         <h2 className="text-3xl">{meta.slug}</h2>
         <br />
         <p className="text-md">by {meta.author}</p>
+        <br />
         <br />
         {children}
         <br />
