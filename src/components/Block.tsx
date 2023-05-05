4ce8ee04cc138dd0d48f5892f6ccd5ce18d63ed0
hachem betrouni <hb@big-mama.io>
2023-05-05 12:50:03 +0100

    update blocks

--- src/components/Block.tsx
+++ src/components/Block.tsx
@@ -12,6 +12,16 @@ type BlockProps = {
   className?: string;
   dummy?: boolean;
 };
+const BlockText = ({ name, description, date }: BlockProps) => {
+  return (
+    <div className="relative py-6 px-9 min-h-[25vh]">
+      <h1 className="text-5xl">{name}</h1>
+      <br />
+      <p className="text-2xl">{description}</p>
+      <p className="text-xl absolute right-5 bottom-5">{date}</p>
+    </div>
+  );
+};
 
 const Block = ({
   name,
@@ -22,36 +32,37 @@ const Block = ({
   dummy,
 }: BlockProps) => {
   const [play] = useSound(sound, { volume: 0.25 });
-  return (
-    <motion.div
-      onClick={() => {
-        play();
-      }}
-      initial={{ opacity: 0, scale: 0.8 }}
-      animate={{ opacity: 1, scale: 1 }}
-      transition={{ duration: 0.2 }}
-      whileHover={
-        dummy
-          ? {}
-          : {
-              scale: [1, 1.05],
-              transition: { duration: 0.1 },
-            }
-      }
-      whileTap={{ scale: 0.95 }}
-      className={`${className} col-span-1 row-span-1 rounded-3xl backdrop-blur-3xl dark:backdrop-blur-3xl border-2 border-black dark:border-white`}
-    >
-      <a href={link}>
-        <div className="relative py-6 px-9 min-h-[25vh]">
-          {dummy ? <></> : <ArrowUpRight className="absolute top-2 right-2" />}
-          <h1 className="text-5xl">{name}</h1>
-          <br />
-          <p className="text-2xl">{description}</p>
-          <p className="text-xl absolute right-5 bottom-5">{date}</p>
-        </div>
-      </a>
-    </motion.div>
-  );
+  if (dummy) {
+    return (
+      <div
+        className={`${className} col-span-1 row-span-1 rounded-3xl backdrop-blur-3xl dark:backdrop-blur-3xl border-2 border-black dark:border-white`}
+      >
+        <BlockText name={name} description={description} date={date} />
+      </div>
+    );
+  } else {
+    return (
+      <motion.div
+        onClick={() => {
+          play();
+        }}
+        initial={{ opacity: 0, scale: 0.8 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.2 }}
+        whileHover={{
+          scale: [1, 1.05],
+          transition: { duration: 0.1 },
+        }}
+        whileTap={{ scale: 0.95 }}
+        className={`${className} col-span-1 row-span-1 rounded-3xl backdrop-blur-3xl dark:backdrop-blur-3xl border-2 border-black dark:border-white`}
+      >
+        <a href={link}>
+          <ArrowUpRight className="absolute top-2 right-2" />
+          <BlockText name={name} description={description} date={date} />
+        </a>
+      </motion.div>
+    );
+  }
 };
 
 export default Block;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,15 @@
 import Block from "@/components/Block";
 import { motion } from "framer-motion";
+
 export default function Grid() {
   return (
     <motion.div className="p-5 grid grid-cols-1 md:grid-cols-5  gap-x-8 gap-y-5">
       <Block
         name="welcome to hachem.xyz"
-        description="more than a portfilio, this is my internet presence !"
+        description="more than a portfolio, this is my internet presence !"
         link=""
         dummy={true}
-        className="opacity-0 dark:text-white text-black md:col-span-5"
+        className="dark:text-white text-black md:col-span-5"
       />
       <Block
         name="About me"
@@ -20,21 +21,21 @@ export default function Grid() {
       <Block
         name="Projects"
         description="cool projects I worked on during hackathons, freelance jobs and in my free-time"
-        link="projects"
+        link=""
         className="bg-gray-300 dark:bg-zinc-500 md:col-span-2"
       />
 
       <Block
         name="Blogs"
         description="click here Mom !"
-        link="blogs"
+        link="content/blogs"
         className="bg-purple-300 dark:bg-sky-600"
       />
 
       <Block
         name="Get in touch"
         description=""
-        link="contact"
+        link=""
         className="bg-green-300 dark:bg-green-800 "
       />
 
