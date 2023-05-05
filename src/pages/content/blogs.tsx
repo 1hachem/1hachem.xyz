fa13fdb457ad0259a53a660d5af11943c8b657af
hachem betrouni <hb@big-mama.io>
2023-05-05 12:50:16 +0100

    move blogs

--- src/pages/content/blogs.tsx
+++ src/pages/content/blogs.tsx
@@ -2,7 +2,7 @@ import Block from "@/components/Block";
 export default function Blogs() {
   return (
     <div className="p-5 grid md:grid-cols-3 gap-x-8 gap-y-5 ">
-      <Block
+      {/* <Block
         name="1 million reasons"
         description="why I never owned a smartphone"
         date="12/12/2023"
@@ -29,6 +29,13 @@ export default function Blogs() {
         date="12/12/2023"
         link="/blogs/empty"
         className="bg-green-300 dark:bg-green-900 md:col-span-2"
+      /> */}
+      <Block
+        name="automatic prototyping"
+        description="blog for indabaX algeria 2023 poster"
+        date="05/05/2023"
+        link="/blogs/autoprototype"
+        className="bg-slate-400 dark:bg-zinc-200 text-white dark:text-black"
       />
     </div>
   );
