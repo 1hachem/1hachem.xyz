2cb49ed7dbddd55cd82bcedf8e5363ae9ce59ae1
hachem betrouni <hb@big-mama.io>
2023-05-07 12:47:47 +0100

    add profile picture

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -4,15 +4,20 @@ import { useSession, signIn, signOut } from "next-auth/react";
 export default function Profile() {
   const { data: session } = useSession();
   if (session?.user) {
+    const { image } = session.user;
+    console.log(image);
     return (
-      <button
-        onClick={() => signOut()}
-        data-te-toggle="tooltip"
-        title={`signed in as ${session.user.email} click to sign out`}
-        className="text-black dark:text-white hover:text-yellow-400 dark:hover:text-yellow-400"
-      >
-        <Lock />
-      </button>
+      <>
+        <button
+          onClick={() => signOut()}
+          data-te-toggle="tooltip"
+          title={`signed in as ${session.user.email} click to sign out`}
+          className="text-black dark:text-white hover:text-yellow-400 dark:hover:text-yellow-400"
+        >
+          <Lock />
+        </button>
+        <img src={image!} className="rounded-full w-6 h-6" />
+      </>
     );
   }
   return (
