776790edff1c43add0ec5927ea8bcf4db21c4d0c
hachem betrouni <hb@big-mama.io>
2023-05-08 09:52:10 +0100

    remove blog

--- src/pages/content/blogs.tsx
+++ src/pages/content/blogs.tsx
@@ -2,14 +2,15 @@ import Block from "@/components/Block";
 export default function Blogs() {
   return (
     <div className="p-5 grid md:grid-cols-3 gap-x-8 gap-y-5 ">
-      {/* <Block
+      <Block
         name="1 million reasons"
         description="why I never owned a smartphone"
         date="12/12/2023"
         link="/blogs/smartphone"
         className="bg-slate-400 dark:bg-zinc-200 text-white dark:text-black"
       />
-      <Block
+
+      {/* <Block
         name="Definitive guide"
         description="for wining freelance bids"
         date="12/12/2023"
@@ -30,13 +31,13 @@ export default function Blogs() {
         link="/blogs/empty"
         className="bg-green-300 dark:bg-green-900 md:col-span-2"
       /> */}
-      <Block
-        name="automatic prototyping"
-        description="blog for indabaX algeria 2023 poster"
-        date="05/05/2023"
-        link="/blogs/autoprototype"
-        className="bg-slate-400 dark:bg-zinc-200 text-white dark:text-black"
-      />
+      {/* <Block
+      //   name="automatic prototyping"
+      //   description="blog for indabaX algeria 2023 poster"
+      //   date="05/05/2023"
+      //   link="/blogs/autoprototype"
+      //   className="bg-slate-400 dark:bg-zinc-200 text-white dark:text-black"
+      // /> */}
     </div>
   );
 }
