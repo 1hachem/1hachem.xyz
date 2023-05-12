9279439a24ed7df49beda2a96789b1e55f39ae25
hachem betrouni <hb@big-mama.io>
2023-05-12 16:39:15 +0100

    add sound to nav elements

--- src/components/NavElement.tsx
+++ src/components/NavElement.tsx
@@ -1,4 +1,7 @@
 import Link from "next/link";
+import useSound from "use-sound";
+
+const sound = "/sounds/hover.mp3";
 
 type ElementProps = {
   name: string;
@@ -8,12 +11,16 @@ type ElementProps = {
 };
 
 const NavElement = ({ name, icon, link, className }: ElementProps) => {
+  const [play] = useSound(sound, { volume: 0.5 });
   return (
     <Link
       className={`${className} hover:text-slate-600`}
       href={link!}
       data-te-toggle="tooltip"
       title={name}
+      onClick={() => {
+        play();
+      }}
     >
       {icon}
     </Link>
