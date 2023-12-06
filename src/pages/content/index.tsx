8bf8642b86e5722afd54470e0d2ab8c01ee34268
1hachem <hachem.betrouni@g.enp.edu.dz>
2023-12-06 23:28:26 +0100

    feat: update blogs page

--- src/pages/content/index.tsx
+++ src/pages/content/index.tsx
@@ -1,5 +1,12 @@
-import Block from "@/components/Block";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { getContentWithMeta } from "@/utils/getContent";
+import Link from "next/link";
 
 export const getStaticProps = () => {
   const { slugs, contentMeta } = getContentWithMeta();
@@ -11,7 +18,7 @@ export const getStaticProps = () => {
   };
 };
 
-export default function Content({
+export default function Blogs({
   slugs,
   contentMeta,
 }: {
@@ -28,13 +35,15 @@ export default function Content({
     <div className="p-5 grid md:grid-cols-3 gap-x-8 gap-y-5 ">
       {contentMeta.map((meta, index) =>
         meta.publish ? (
-          <Block
-            key={meta.title}
-            name={meta.title}
-            description={meta.description}
-            link={`content/${slugs[index]}`}
-            className={meta.className}
-          />
+          <Link href={`content/${slugs[index]}`}>
+            <Card className="hover:scale-105 shadow-md">
+              <CardHeader>
+                <CardTitle>{meta.title}</CardTitle>
+                <CardDescription>{meta.description}</CardDescription>
+              </CardHeader>
+              <CardContent></CardContent>
+            </Card>
+          </Link>
         ) : (
           <></>
         )
