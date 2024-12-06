938971d8280b6bf6ddee96f72c0f0982ee8b85ac
1hachem <hb@big-mama.io>
2024-12-06 04:21:52 +0100

    perf: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,14 @@ import { remarkReadingTime } from "./remark-reading-time.mjs";
 import { remarkModifiedTime } from "./remark-modified-time.mjs";
 import tailwind from "@astrojs/tailwind";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: "https://1hachem.xyz",
+  integrations: [tailwind(), sitemap()],
   markdown: {
     remarkPlugins: [remarkReadingTime, remarkModifiedTime],
   },
+  prefetch: true,
 });
