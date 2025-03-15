a7b00a7eddab4dbdbbb485eadfd43044b8b4d52d
1hachem <hb@big-mama.io>
2025-03-16 00:32:45 +0100

    feat: add callouts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,42 @@
 // @ts-check
+import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import remarkCallout from "@r4ai/remark-callout";
+
 import { defineConfig } from "astro/config";
 import { remarkModifiedTime } from "./remark-modified-time.mjs";
 import { remarkReadingTime } from "./remark-reading-time.mjs";
 
-import sitemap from "@astrojs/sitemap";
-
-import mdx from "@astrojs/mdx";
-
-// https://astro.build/config
 export default defineConfig({
   site: "https://1hachem.xyz",
   integrations: [react(), tailwind(), sitemap(), mdx()],
   markdown: {
-    remarkPlugins: [remarkReadingTime, remarkModifiedTime],
+    remarkPlugins: [
+      remarkReadingTime,
+      remarkModifiedTime,
+      [
+        remarkCallout,
+        {
+          root: () => ({
+            tagName: "callout",
+          }),
+          body: (/** @type {{ type: string;  }} */ callout) => ({
+            tagName: "callout-body",
+            properties: {
+              calloutType: callout.type,
+            },
+          }),
+          title: (/** @type {{ type: string; }} */ callout) => ({
+            tagName: "callout-title",
+            properties: {
+              calloutType: callout.type,
+            },
+          }),
+        },
+      ],
+    ],
   },
   prefetch: true,
   devToolbar: { enabled: false },
