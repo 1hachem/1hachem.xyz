8c7229b7401b007e846f47c8e44952177143dec2
1hachem <hb@big-mama.io>
2025-03-17 01:14:46 +0100

    chore: add tags to blogs

--- src/content.config.ts
+++ src/content.config.ts
@@ -6,6 +6,7 @@ const blog = defineCollection({
   schema: z.object({
     title: z.string(),
     description: z.string(),
+    tag: z.enum(["lifestyle", "dev", "relegion", "management"]),
   }),
 });
 
