d045bc276b659b701c9fa04a20413db15a7c5383
1hachem <hb@big-mama.io>
2025-04-18 09:56:15 +0100

    feat: migrate to tailwind 4 fix font issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,26 @@
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
 import remarkCallout from "@r4ai/remark-callout";
 
 import vercel from "@astrojs/vercel";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 import { remarkModifiedTime } from "./remark-modified-time.mjs";
 import { remarkReadingTime } from "./remark-reading-time.mjs";
 
 export default defineConfig({
   site: "https://1hachem.xyz",
-  integrations: [react(), tailwind(), sitemap(), mdx()],
+  integrations: [react(), sitemap(), mdx()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 
   markdown: {
+    shikiConfig: {
+      // theme: syntaxTheme,
+      wrap: true,
+    },
     remarkPlugins: [
       remarkReadingTime,
       remarkModifiedTime,
@@ -42,6 +49,5 @@ export default defineConfig({
 
   prefetch: true,
   devToolbar: { enabled: false },
-
   adapter: vercel(),
 });
